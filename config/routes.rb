0d13c1442c6606fcd46bbd7d7a29ab39dc536bdd
Kirill Kuprikov <kkuprikov@gmail.com>
2015-12-10 18:11:30 +0300

    Added task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
   root to: 'visitors#index'
   devise_for :users
-  resources :users
+  resources :users, :tasks
+  get '/new_task' => 'tasks#new'
 end
